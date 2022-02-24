8fd0c7087c3507fc04e62c43ffb485ee56fe24cf
Annie <devannie777@gmail.com>
2022-02-24 05:32:48 +0000

    UseEffect with dependancy array exercise

--- src/App.js
+++ src/App.js
@@ -7,39 +7,27 @@ import './index.css';
 //import Checkbox from "./Checkbox.js";
 
 function App() {
-  const [value, setValue] = useState("");//1)Add inital state and useState function. 2) Tie initial state to input
-  const [value2, setValue2] = useState("");//1)Add inital state and useState function
-  /*3) To change state use onChange() i.e so every
-      time I type into it it will collect the value
-  4)use useEffect to f.ex console.log value of each input/values in below fields
-  5) BUT only want one field to update at a time so need to use a
-  dependency array. Right at end of useEffect function in []
-  So if we put [value] in here it will only fire useEffect for first field BUT CAN PASS BOTH FIELDS IN TO IF NEEDED LIKE THIS:
-  useEffect(() => {
-    console.log(`field 1: ${value}`);
-  }, [value, value2]);
-  */
-  
-  useEffect(() => {
-    console.log(`field 1: ${value}`);
-  }, [value]);{/*5) */}
-  
-  useEffect(() => {
-    console.log(`field 2: ${value2}`);
-  }, [value2]);{/*5) */}
-  
+  const [value1, setValue1] = useState("");
+  const [value2, setValue2] = useState("");
+
+  useEffect(() =>{
+    console.log(`Favourite animal: ${value1}`)
+  }, [value1]);
+
+  useEffect(() =>{
+    console.log(`Favourite animal: ${value2}`)
+  }, [value2]);
   return (
     <>
       <label>
-        Favourite Phrase:
-        <input value={value} onChange={e => setValue(e.target.value)}/>{/*2)Tie value/i state to input */}{/*3) */}
+        Favourite Animal: 
+        <input value={value1} onChange={e => setValue1(e.target.value)}/>
       </label>
       <br />
       <label>
-        Second Favourite Phrase:
-        <input value={value2} onChange={e => setValue2(e.target.value)}/>{/*2) Tie value2/i state to input */}{/*3) */}
+        Favourite Animal: 
+        <input value={value2} onChange={e => setValue2(e.target.value)}/>
       </label>
-
     </>
   );
 }
