58dc33b222ab4858a0fd1cdfd7b8c47daf2b4e8c
Annie <devannie777@gmail.com>
2022-03-03 05:19:58 +0000

    UseReducer Hook exercise

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 //import logo from './logo.svg';
 
-//import React, { useState, useEffect } from "react";
-import React from 'react';
+import React, { useState, useEffect } from "react";
+//import React from 'react';
 import './App.css';
 import './index.css';
-import Checkboxes from "./My Folder/Exercises/Checkboxes.js";
+import ApiGithub from "./My Folder/Exercises/ApiGithub.js";
 
-//import UseReducer from "./My Folder/UseReducer.js"
+
 
 function App() {
   
@@ -14,7 +14,7 @@ function App() {
   
   return (
     <div>
-      <Checkboxes />
+     <ApiGithub />
       
       
       
---
+++ src/My Folder/Exercises/ApiGithub.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function ApiGithub() {
+  const [data, setData] = useState(null);
+  
+  return (
+    <div>
+
+
+    </div>
+  )
+}
+
+export default ApiGithub;
