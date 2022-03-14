938f3f3e264d7bacfebdb48a75a374ce0396e06c
Annie <devannie777@gmail.com>
2022-03-14 05:58:47 +0000

    Compnent exercise

--- src/App.js
+++ src/App.js
@@ -1,18 +1,12 @@
 //import logo from './logo.svg';
 
-import React, { useState, useEffect } from "react";
-//import React from 'react';
+//import React, { useState, useEffect } from "react";
+import React from 'react';
+import ComponentEx from './React for Rest of Us/ComponentEx';
 import './App.css';
 import './index.css';
 
-/*
-Differences btwn HTML and jsx
-1)React fragment <></> so React won't output an
-unnecessary div in your mark-up but but still need
-1 top-level element
-2)Use of className
-3)Use {} when include js in jsx
-4)<OurApp />*/
+
 
 
 function App() {
@@ -21,13 +15,9 @@ function App() {
   
   return (
     <>
-      <h1>Our Amazing App Header</h1>
+      <ComponentEx />
       <p>The current time is {new Date().toLocaleString()}</p>
       <small>Copyright Footer Text</small>
-      
-      
-      
-      
     </>
     
   );
---
+++ src/React for Rest of Us/ComponentEx.js
@@ -0,0 +1,21 @@
+import React from 'react'
+
+function ComponentEx() {
+  return (
+    <div>
+      <h1>Our Amazing App</h1>
+      <p>More Header text....</p>
+    </div>
+  )
+}
+
+export default ComponentEx;
+
+/*or if only 1
+import React from 'react'
+
+function ComponentEx() {
+  return <h1>Our Header Component</h1>
+}
+
+export default ComponentEx;*/
