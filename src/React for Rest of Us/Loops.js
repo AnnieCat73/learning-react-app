5e2367e03f2ce9f8ed2ca79c9542077ac884b0fc
Annie <devannie777@gmail.com>
2022-03-20 05:39:31 +0000

    Looping with map in react

--- src/React for Rest of Us/Loops.js
+++ src/React for Rest of Us/Loops.js
@@ -1,6 +1,6 @@
 import React from 'react'
 
-//Loops with forEach and map
+//Loops with for of and map
 
 function Loops() {
 
@@ -8,17 +8,28 @@ function Loops() {
   //The entries() method returns an Array Iterator object with key/value pairs
   const elements = ['one', 'two', 'three'];
   const items = [];
-  for (const [index, value] of elements.entries()) {
-    items.push(<li key={index}>{value}</li>)
+  for (const [index, element] of elements.entries()) {
+    items.push(<li key={index}>{element}</li>)
   }
-
   //map
+  //1
+  const numbers = elements.map((element, index) => {
+    return <li key={index}>{element}</li>
+  })
 
   return (
     <>
       <div>
         {items}
-    </div>
+      </div>
+      <div>
+        {numbers}
+      </div>
+      <div>{/*2) or directly in jsx*/}
+        {elements.map((index, element) => {
+          return <li key={index}>{element}</li>
+          })}
+      </div>
     </>
     
   )
